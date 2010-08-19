3c4e6f9ab98a2021fa96783d05a1f9d482f75414
stephenmcd <steve@jupo.org>
2010-08-19 11:06:44 +1000

    Added MANIFEST and setup files.

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+ 
+ 
+setup(
+    name = "django-shotgun",
+    version = __import__("django_shotgun").__version__,
+    author = "Stephen McDonald",
+    author_email = "stephen.mc@gmail.com",
+    description = ("A Django application for testing entire sites."),
+    long_description = open("README.rst").read(),
+    url = "http://github.com/stephenmcd/django-shotgun",
+    zip_safe = False,
+    packages = find_packages(),
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+    ]
+)
+
